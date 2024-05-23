bc2cc97d81b79243180f8e3f937f216807b7910c
Jakari Robinson <44513180+Jtrobinson2@users.noreply.github.com>
2024-05-23 18:58:28 -0400

    Fixed regression in get cinema items

--- src/web_controller/web_actions.py
+++ src/web_controller/web_actions.py
@@ -226,8 +226,8 @@ def getCinemaItems(driver : webdriver, cinemaItemTitle : str) -> list[str]:
     actions.click()
     
     actions.perform()
+    searchResultListItems = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div[2]/div[4]/section/div/div[1]/section[2]/div[2]/ul/li/div[2]/div/a")
 
-    searchResultListItems = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div[2]/div[3]/section/div/div[1]/section[2]/div[2]/ul/li[contains(@class, 'ipc-metadata-list-summary-item ipc-metadata-list-summary-item--click find-result-item find-title-result')]/div[2]/div/a")
     return  [item.text for item in searchResultListItems]
 
 
