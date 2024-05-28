cdd2bf88483bff8debcd5a67e4ab47b43d6ad40f
Jakari Robinson <44513180+Jtrobinson2@users.noreply.github.com>
2024-05-28 18:59:14 -0400

    Chrome opens correct profile added configuration to store this profile (doesn't run code aftewards though)

--- src/main.py
+++ src/main.py
@@ -11,14 +11,23 @@ from src.web_controller import web_actions
 from src.model.review import Review
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from src.util import Configuration
 
+
+#TODO correct profile is opened but session not created exception persists will dry downloading and updating chrome driver
+"""    raise exception_class(message, screen, stacktrace)
+selenium.common.exceptions.SessionNotCreatedException: Message: session not created: Chrome failed to start: exited normally.
+  (session not created: DevToolsActivePort file doesn't exist)
+  (The process started from chrome location C:\Program Files (x86)\Google\Chrome\Application\chrome.exe is no longer running, so ChromeDriver is assuming that Chrome has crashed.)"""
 chrome_options = Options()
 chrome_options.add_experimental_option("useAutomationExtension", False)
 chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument(f"--user-data-dir={Configuration.PATH_TO_CHROME_PROFILE}")
+chrome_options.add_argument('--profile-directory=Profile 3')
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
 driver = webdriver.Chrome(options=chrome_options)
 
-web_actions.login(driver, "03jrob@gmail.com", "testpass", "TestAccount")
+# web_actions.login(driver, "03jrob@gmail.com", "testpass", "TestAccount")
+web_actions.isLoggedIn(driver)
 
-
-#TODO check the coverage of the tests before moving on to the ui
---
+++ src/util/Configuration.py
@@ -0,0 +1,2 @@
+"""Temp file to hold configurations"""
+PATH_TO_CHROME_PROFILE = r"C:\Users\Jtrob\AppData\Local\Google\Chrome\User Data"
